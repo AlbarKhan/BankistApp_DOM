0e89b844039b2d08202cff1e3e96108017b99a4a
unknown <albarkhan60@gmail.com>
2024-01-02 14:49:32 +0530

    modal functionality added

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--open-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    openModal();
+  });
+});
+
+btnCloseModal.addEventListener("click", closeModal);
